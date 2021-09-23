d83e5d8ad498cb914d6c8cf4665a47d3220a1e24
adultlee <seoungin1228@gmail.com>
2021-09-23 21:57:31 +0900

    feat: calculation crc

--- main.cpp
+++ main.cpp
@@ -2,8 +2,127 @@
 #include <vector>
 
 using namespace std;
+int inputArr[14]; // 14의 크기를 가진 배열로 초기 입력되는 수이다. 
+int copyArr[17]; //17의 크기(14+3)를 가진 crc 결과가 더해져 만들어질 배열이다.
+int tempArr[17]; // 중간 계산과정에 있어서 crc도출해 내기 위한 배열이다.
+int crc[3]; // 3의 크기를 가진 crc 결과이다. 초기에는 0으로 초기화 되어있다. 
+int divisor[4];
 
+void div()
+{
+    for (int i = 0; i < 14; i++)
+    {
+        if (tempArr[i] == 1)
+        {
+            //가장 첫위치
+            if (tempArr[i] == divisor[0])
+            {
+                tempArr[i] = 0;
+            }
+            else
+            {
+                tempArr[i] = 1;
+            }
+            //두번째 위치
+            if (tempArr[i + 1] == divisor[1])
+            {
+                tempArr[i + 1] = 0;
+            }
+            else
+            {
+                tempArr[i + 1] = 1;
+            }
+
+            //3번째 위치
+            if (tempArr[i + 2] == divisor[2])
+            {
+                tempArr[i + 2] = 0;
+            }
+            else
+            {
+                tempArr[i + 2] = 1;
+            }
+            //4번째 위치
+            if (tempArr[i + 3] == divisor[3])
+            {
+                tempArr[i + 3] = 0;
+            }
+            else
+            {
+                tempArr[i + 3] = 1;
+            }
+
+            for (int j = 0; j < 17; j++)
+            {
+
+                cout << tempArr[j] << " ";
+            }
+            cout << endl;
+        }
+    }
+}
 int main()
 {
-    cout << "Entered Test Number"<<endl;
+    cout << "샘플 데이터를 입력하시오"<<endl;
+    for(int i=0; i<14;i++ )
+    {
+        cin >> inputArr[i];
+    }
+   
+    cout << "제수를 입력하시오" <<endl;
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> divisor[i];
+    }
+   
+    cout << endl;
+    cout << "------------------------------------------------------"<<endl;
+    cout << "나눠질 배열입니다. 14번째 이후에는  0 으로 초기화 해줍니다. "<<endl;
+    for (int i = 0; i < 17; i++)
+    {
+        if(i<14)
+        {
+            tempArr[i] = inputArr[i];
+        }
+        else
+        {
+            tempArr[i] = 0;
+        }
+        cout << " "<<tempArr[i];
+    }
+
+    cout << endl;
+    cout << "------------------------------------------------------" << endl;
+    cout << "제수를 통해서 임시 배열이 나눠지는 과정입니다. 이진수의 나눗셈은 Xor 논리로 이루어집니다." << endl;
+    
+    div();
+
+    cout << endl<< "------------------------------------------------------" << endl;
+    cout << "최종으로 나눠진 배열입니다. 마지막 세 숫자는 CRC배열에 들어가게 됩니다." << endl;
+    cout <<endl<< "나누는 과정 후 arr" <<endl;
+    for (int i = 0; i < 17; i++)
+    {
+        cout << tempArr[i] <<" ";
+    }
+   
+    crc[0] = tempArr[14];
+    crc[1] = tempArr[15];
+    crc[2] = tempArr[16];
+
+    cout << endl;
+
+    cout << "------------------------------------------------------" << endl<<endl;
+    cout << "당신의 제수(divisor)는 " ;
+    for(int i = 0; i<4; i++)
+    {
+        cout << " " << divisor[i];
+    }
+    cout << "\n당신의 CRC bit 는  ";
+    for(int i = 0; i<3; i++)
+    {
+        cout << " " << crc[i];
+    }
+    cout << endl;
+
+
 }
